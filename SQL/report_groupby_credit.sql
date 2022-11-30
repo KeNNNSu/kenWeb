9d5eb80e35c460b327542dc69538c18c53957b90
ken05987 <ken22515368@gmail.com>
2022-11-30 23:04:59 +0800

    修正SQL語法

--- SQL/report_groupby_credit.sql
+++ SQL/report_groupby_credit.sql
@@ -1,4 +1,5 @@
-SELECT s.id, s.code, s.name AS credit, sum(j.amount) AS subtotal
+CREATE VIEW REPORT_GROUPBY_CREDIT
+AS SELECT s.id, s.code, s.name AS credit, sum(j.amount) AS subtotal
 FROM acc_subjects s, acc_journals j
 WHERE j.credit = s.id
-GROUP BY s.id, s.code, s.name
+GROUP BY s.id, s.code, s.name;
--- SQL/report_groupby_debit.sql
+++ SQL/report_groupby_debit.sql
@@ -1,4 +1,5 @@
-SELECT s.id, s.code, s.name AS debit, sum(j.amount) AS subtotal
+CREATE VIEW REPORT_GROUPBY_DEBIT
+AS SELECT s.id, s.code, s.name AS debit, sum(j.amount) AS subtotal
 FROM acc_subjects s, acc_journals j
 WHERE j.debit = s.id
-GROUP BY s.id, s.code, s.name
+GROUP BY s.id, s.code, s.name;
